541f3e517af03d04877eff29b48c3a077b96da07
ivan <fatai.fakoya@gmail.com>
2018-11-27 17:18:08 -0500

    added explosions for notebooks, moving cat and food
    Co-Authored-By: Zachariyya Khan  <zackkhan@users.noreply.github.com>

---
+++ StressRelievAR/Assets/ExplosionScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+	public GameObject Explosion;
+	public GameObject Notebook;
+	public GameObject g;
+	public bool goingRight = true;
+
+	void Explode()
+	{
+		var exp = GetComponent<ParticleSystem>();
+		exp.Play();
+		Destroy(gameObject, exp.duration / 2);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("COLLISION");
+		if (other.gameObject.name.Contains("notepad") != true)
+		{
+			var exp = other.gameObject.GetComponent<ParticleSystem>();
+			exp.Play();
+			Destroy(other.gameObject, exp.duration);
+		}
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		// GameObject obj2 = Instantiate(Explosion, Notebook.transform.position, Quaternion.identity);
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!goingRight)
+		{
+			g.transform.Translate(0.1f, 0, 0);
+			g.GetComponent<BoxCollider>().transform.Translate(0.1f, 0, 0);
+
+		}
+		else
+		{
+			g.transform.Translate(-0.1f, 0, 0);
+			g.GetComponent<BoxCollider>().transform.Translate(-0.1f, 0, 0);
+		}
+
+		if (g.transform.position.x > 15)
+		{
+			goingRight = false;
+		}
+		else if (g.transform.position.x < -15)
+		{
+			goingRight = true;
+		}
+	}
+
+	void move(GameObject g)
+	{
+		bool goingRight = true;
+		if (goingRight)
+		{
+			g.transform.Translate(0.01f, 0, 0);
+			g.GetComponent<BoxCollider>().transform.Translate(0.01f, 0, 0);
+
+		}
+		else
+		{
+			g.transform.Translate(-0.01f, 0, 0);
+			g.GetComponent<BoxCollider>().transform.Translate(-0.01f, 0, 0);
+		}
+
+		if (g.transform.position.x > 20)
+		{
+			goingRight = false;
+		}
+		else if (g.transform.position.x < -20)
+		{
+			goingRight = true;
+		}
+	}
+}

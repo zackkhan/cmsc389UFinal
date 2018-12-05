34a72bbc1f762880525da3727e22ccd73d502ef7
zoeyleqa <zoeyleqa@gmail.com>
2018-12-05 12:07:45 -0500

    Added destructive porcelain items
    porcelains break on collision

---
+++ StressRelievAR/Assets/destroyOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOnCollision : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject BrokenVersion;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.relativeVelocity.magnitude > 2) { 
+            Instantiate(BrokenVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        
+    }
+    // Update is called once per frame
+    void Update () {
+
+        
+    }
+}

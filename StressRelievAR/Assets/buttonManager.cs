58c18c21459b7903a5b4a8fdaa32bf752f6b5d0c
zoeyleqa <qal@terpmail.umd.edu>
2018-12-07 12:49:37 -0500

    added buttonManager, voiceinputmanager
    added scripts to control porcelains

---
+++ StressRelievAR/Assets/buttonManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.Receivers;
+using HoloToolkit.Unity.InputModule;
+
+
+public class buttonManager : InteractionReceiver
+{
+    private string[] AllTags = { "Food", "Porcelain", "Explosive", "BrokenPieces" };
+
+    private void Start()
+    {
+        
+    }
+    public void turnOffVisibility(string objTag)
+    {
+        GameObject[] allObjWithTag = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject obj in allObjWithTag)
+        {//could make a destroy object version of this
+            obj.SetActive(false);
+        }
+    }
+
+    public void turnOnVisibility(string OnObjTag)
+    {
+        foreach(string tag in AllTags){
+            GameObject[] allObjWithTag = GameObject.FindGameObjectsWithTag(tag);
+            foreach(GameObject obj in allObjWithTag)
+            {
+                if (Equals(tag, OnObjTag))
+                {
+                    obj.SetActive(true);
+                } else
+                {
+                    obj.SetActive(false);
+                }
+            }
+            
+        }
+    }
+    protected override void InputDown(GameObject obj, InputEventData eventData)
+    {
+        Debug.Log(obj.name + "Input Down!");
+
+        switch (obj.name)
+        {
+            case "PorcelainMode":
+                turnOnVisibility("Porcelain");
+                break;
+
+            case "button2":
+                turnOnVisibility("Food");
+                turnOffVisibility("BrokenPieces"); 
+                break;
+
+            case "button3":
+                turnOnVisibility("Explosive");
+                turnOffVisibility("BrokenPieces");
+                break;
+        }
+    }
+}
